5659cf3a8cd7014604d0c46e6863480535105e4a
lumiknit <aasr4r4@gmail.com>
2023-05-31 08:07:23 +0900

    Add naive logging

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,47 @@
 use axum::{
     routing::get,
+    http::StatusCode,
     Router,
 };
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
+
+fn new_api_router() -> Router {
+    Router::new()
+        .route("/healthz", get(|| async { "OK" }))
+        .route("/hello", get(|| async { "Hello, World!" }))
+        .route("/bad", get(|| async { (StatusCode::NOT_FOUND, "BOOM") }))
+}
+
+fn new_static_router() -> Router {
+    Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+}
+
+fn new_router() -> Router {
+    Router::new()
+        .nest("/-", new_api_router())
+        .nest("/", new_static_router())
+        .layer(TraceLayer::new_for_http()
+            .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+            .on_response(trace::DefaultOnResponse::new().level(Level::INFO)))
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
+    // Configurations
+    let host: String = "0.0.0.0".to_string();
+    let port: u16 = 3000;
+
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
+    // Create and run server
+    tracing::info!("Listening on http://{}:{}", host, port);
+    axum::Server::bind(&format!("{}:{}", host, port).parse().unwrap())
+        .serve(new_router().into_make_service())
         .await
         .unwrap();
 }
